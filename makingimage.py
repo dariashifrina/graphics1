6e6dd51743185e9d3232f0477a8bae006d99a891
dariashifrina <dshifrina@stuy.edu>
2018-02-04 17:45:49 -0500

    future hair color done

--- makingimage.py
+++ makingimage.py
@@ -8,21 +8,39 @@ def crt_ppm(name, length, width, maxpix, content):
     print " ... Done"
 
 def ppm_content():
+    a = 233
+    b = 201
+    c = 255
+    d = 255
+    e = 179
+    f = 232
+    g = 163
+    h = 86
+    i = 89
     content = ""
     for i in range(0,500):
         if(i%3 == 0):
             for m in range(0,500):
-                content += "49 34 255 "
+                content += str(a%255) + " " + str(b%255) + " " + str(c%255) + " "
+                a +=1
+                b+=1
+                c+=1
                 m += 1
         elif(i%2 == 0):
             for x in range(0,500):
-                content += "149 128 255 "
+                content += str(d%255) + " " + str(e%255) + " " + str(f%255) + " "
+                d +=1
+                e +=1
+                f +=1
                 x += 1
         else:
             for z in range(0,500):
-                content += "255 77 228 "
+                content += str(f%255) + " " + str(g%255) + " " + str(h%255) + " "
+                f +=1
+                g +=1
+                h +=1
                 z += 1
         i += 1
     return content
 
-crt_ppm("woo", 500, 500, 255, ppm_content())
+crt_ppm("future_hair_color", 500, 500, 255, ppm_content())
